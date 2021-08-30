a0cddc98213159dd765fcd06f72aa55b33a62c2b
Pedro Gaya <pedrogaya@gmail.com>
2021-08-30 00:24:23 -0300

    change size of body

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const rotasCliente = require('./rotas/rotasCliente');
 const rotasRestaurantes = require('./rotas/rotasRestaurante');
 const app = express();
 
-app.use(express.json({ limit: '5mb' }));
 app.use(cors());
+app.use(express.json({ limit: '20mb' }));
 app.use(rotasCliente);
 app.use(rotasRestaurantes);
 app.listen(process.env.PORT || 3000);
